a21d4a65616401037a701da69bb60da3b78507a8
Adelina Radulescu <adel0896@stud.kea.dk>
2022-09-20 14:54:37 +0200

    created the form

--- app.js
+++ app.js
@@ -6,26 +6,27 @@ const port = 8081;
 
 const uri = "mongodb+srv://traveldestinations:traveldestinations1234@travelcluster.xpbsjto.mongodb.net/destinations";
 const client = new mongodb(uri);
+const path = require("path");
+// async function run() {
+//   try {
+//     const database = client.db("TravelDestinations");
+//     const names = database.collection("destinations");
+//     const exampleObject = { name: "Fuji", location: "Japan", start_date: "12.01.2022", end_date: "13.05.2022", description: "alalalalalalalala", img_url: "img.jpg" };
 
-async function run() {
-  try {
-    const database = client.db("TravelDestinations");
-    const names = database.collection("destinations");
-    const exampleObject = { name: "Fuji", location: "Japan", start_date: "12.01.2022", end_date: "13.05.2022", description: "alalalalalalalala", img_url: "img.jpg" };
-
-    names.insertOne(exampleObject, function (err, info) {
-      console.log("Inserted");
-    });
-  } finally {
-    // Ensures that the client will close when you finish/error
-
-    await client.close();
-  }
-}
+//     names.insertOne(exampleObject, function (err, info) {
+//       console.log("Inserted");
+//     });
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
 
 app.get("/", (req, res) => {
-  res.send("Travel destiations");
-  run().catch(console.dir);
+  // res.send("Travel destiations");
+  res.sendFile(path.join(__dirname + "/index.html"));
+
+  // run().catch(console.dir);
 });
 
 app.listen(port, () => {
