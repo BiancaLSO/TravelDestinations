3a61f0b821e4189f2d02b41205f706407c557ad1
Adelina Radulescu <adel0896@stud.kea.dk>
2022-09-21 19:06:47 +0200

    form adds data to database

--- app.js
+++ app.js
@@ -1,34 +1,61 @@
 const mongodb = require("mongodb").MongoClient;
-
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 8081;
-
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const uri = "mongodb+srv://traveldestinations:traveldestinations1234@travelcluster.xpbsjto.mongodb.net/destinations";
 const client = new mongodb(uri);
 const path = require("path");
-// async function run() {
-//   try {
-//     const database = client.db("TravelDestinations");
-//     const names = database.collection("destinations");
-//     const exampleObject = { name: "Fuji", location: "Japan", start_date: "12.01.2022", end_date: "13.05.2022", description: "alalalalalalalala", img_url: "img.jpg" };
 
-//     names.insertOne(exampleObject, function (err, info) {
-//       console.log("Inserted");
-//     });
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
+async function addAnObject(myObject) {
+  console.log(myObject);
+  try {
+    const database = client.db("TravelDestinations");
+    const names = database.collection("destinations");
+    names.insertOne(myObject, function (err, info) {
+      console.log("Inserted");
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
 
 app.get("/", (req, res) => {
-  // res.send("Travel destiations");
+  res.sendFile(path.join(__dirname + "/create.html"));
+});
+app.post("/create", (req, res) => {
+  const myObject = {
+    name: req.body.name,
+    location: req.body.location,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+    description: req.body.description,
+    img: req.body.img,
+  };
+  res.send("Name is:" + req.body.name);
+  // res.sendFile(path.join(__dirname + "/index.html"));
+  addAnObject(myObject).catch(console.dir);
+});
+app.get("/create", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
-
-  // run().catch(console.dir);
+});
+// this function shows our database at the url /destinations
+app.get("/destinations", (req, resp) => {
+  const database = client.db("TravelDestinations");
+  database
+    .collection("destinations")
+    .find({})
+    .toArray((err, result) => {
+      if (err) throw err;
+      resp.send(result);
+    });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+app.use(express.static("public"));
---
+++ public/javascript/create-script.js
@@ -0,0 +1,25 @@
+//none of it was needed, everything is in app.js through the post request
+
+// const createJS = require("../../app");
+// console.log(createJS);
+// const form = document.querySelector("#createForm");
+// form.addEventListener("submit", async (event) => {
+//   event.preventDefault();
+
+//   const destination = {
+//     name: document.querySelector("#name").value,
+//     location: document.querySelector("#location").value,
+//     startDate: document.querySelector("#startDate").value,
+//     endDate: document.querySelector("#endDate").value,
+//     description: document.querySelector("#description").value,
+//     img: document.querySelector("#img").value,
+//   };
+
+//   const response = createJS.data.postData(destination);
+//   console.log(response);
+//   if (response.status === 200) {
+//     clearForm();
+//     // const newNode = fillContactTemplate(contact);
+//     // displayNewNode(newNode);
+//   }
+// });
